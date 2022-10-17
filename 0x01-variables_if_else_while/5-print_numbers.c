d2e24f0c9fccaddb751ea04cda84b025e6e89bcf
Mary-Maina <richellwanjiru@gmail.com>
2022-10-16 19:48:26 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -print all single digits from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n == 0; n <= 10; n++)
+		putchar((n % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
