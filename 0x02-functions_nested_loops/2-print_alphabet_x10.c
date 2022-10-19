776e58833926c6f2ae20b5af15ab824caca556b0
Mary-Maina <richellwanjiru@gmail.com>
2022-10-18 19:52:36 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0
+	char lett;
+
+	while (count++ <= 9)
+	{
+		for (lett = 'a'; lett <= 'z'; lett++)
+			_putchar(lett);
+		_putchar('\n')
+	}
+}
