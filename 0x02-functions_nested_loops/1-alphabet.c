19a2a296e03d75c3552b68220a36bf66f4e1c861
Mary-Maina <richellwanjiru@gmail.com>
2022-10-19 20:10:28 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar (a);
+
+	_putchar('\n');
+}
