fcdd193a40de2ae67c05242da4543cc66f3304ba
Mary-Maina <richellwanjiru@gmail.com>
2022-10-19 18:39:21 -0700

    Task 9 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,19 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 100; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		putchar(a + '0');
-		if (a < 99)
+		putchar(a);
+		if (a == 9)
+		{
+			break;
+		}
+		else
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
