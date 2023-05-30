9731ac11318e81ab8c99ab0a96a777cd56e6b4d8
Mary-Maina <richellwanjiru@gmail.com>
2023-05-29 17:56:51 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,23 @@ int main(void)
 	int b;
 	int a;
 
-	for (a = 0; a <= 99; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar(a / 10 + '0');
 			putchar(a % 10 + '0');
 			putchar(' ');
 			putchar(b / 10 + '0');
 			putchar(b % 10 + '0');
-			putchar(',');
-
-			if (a != 99 || b != 99)
+			
+			if (a != 98 || b != 99)
 			{
 				putchar(' ');
+				putchar(',');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
