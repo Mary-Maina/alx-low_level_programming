d5fcca9212a3659a4467c93edfe8958afc9eed62
Mary-Maina <richellwanjiru@gmail.com>
2023-06-01 16:29:54 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,7 +40,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((xo / 100) + '0');
-					_putchar(((xo % 100) % 10) + '0');
+					_putchar(((xo / 10) % 10) + '0');
 					_putchar((xo % 10) + '0');
 				}
 			}
