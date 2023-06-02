ae793ea4a30ccbdcb98714c8fbd7b8e4bf085cc0
Mary-Maina <richellwanjiru@gmail.com>
2023-06-01 18:23:25 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
  * Return: 0
  */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	positive_or_negative(i);
 
 	return (0);
