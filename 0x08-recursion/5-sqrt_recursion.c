412170d3e48e217d6e20bfb5d234271a161bfb77
Mary-Maina <richellwanjiru@gmail.com>
2023-07-09 14:58:01 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,48 @@
 #include "main.h"
 /**
- * _sqrt_recursion- Returns the natural square root
- * @n: number to be checked
- * return: -1 if numer does not have natural square root
- */
-int sqt_recursion(int n, int g, int x)
-{
-	int guess = (g + n / g) / 2;
-
-	if (guess < 0)
-	{
-		guess = -guess;
-	}
-	if (guess * guess - n < x)
-	{
-		return (guess);
-	}
-	else
-	{
-		return sqt_recursion(int n, int g, int x);
-	}
-}
-/**
- * _sqrt_recursion- Returns the natural square root
+ * _sqrt_recursion- Returns sqrt of natural nos.
  * @n: number to be checked
  * Return: -1 if numer does not have natural square root
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqRoot(n, 0, n));
+	}
+}
+/**
+ * sqRoot- calculates the square root of a number
+ * @n: The number to be checked
+ * @low: number lower than n
+ * @high: Larger numbers
+ * Return: square root
+ */
+int sqRoot(int n, int low, int high)
+{
+	int x;
 
+	if (low > high)
+	{
+		return (-1);
+	}
+
+	x = (low + high) / 2;
+
+	if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x * x > n)
+	{
+		return (sqRoot(n, low, x - 1));
+	}
+	else
+	{
+		return (sqRoot(n, x + 1, high));
+	}
+}
