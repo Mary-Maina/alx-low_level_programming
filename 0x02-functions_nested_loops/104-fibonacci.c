2564371fe242197c6fef9c92edcd9fc652fb6cd1
Mary-Maina <richellwanjiru@gmail.com>
2023-07-10 09:36:27 -0700

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- Entry Point of program
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int fibonacci[98];
+	int i;
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	printf("%u, %u", fibonacci[0], fibonacci[1]);
+
+	for (i = 0; i < 98; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		printf(", %u", fibonacci[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
