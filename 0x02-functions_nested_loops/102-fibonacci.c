ce1b7185a114c1bfb8c1b649382b42faf183cf37
Mary-Maina <richellwanjiru@gmail.com>
2023-07-10 07:52:47 -0700

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main- prints the sequence of Fibonacci numbers
- * Return: 50 Fibonacci numbers
+ * Return: 0 successful
  */
 int main(void)
 {
 	int fibonacci[50];
+	int i;
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
 	printf("%d, %d", fibonacci[0], fibonacci[1]);
 
-	for (int i = 2; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 
 		printf(", %d", fibonacci[i]);
 	}
 	printf("\n");
+
+	return (0);
 }
