acdc4ece24b49d84137b5bdb423916dcec18b0fa
Mary-Maina <richellwanjiru@gmail.com>
2023-07-10 06:22:29 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n < 1)
 	{
-		if (n > 0)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
