1f16f6c203e15223f78c6116f9fecd1ae1b75caf
Mary-Maina <richellwanjiru@gmail.com>
2023-07-12 03:23:17 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @argc: Number of arguments
+ * @argv: vector arguments
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
