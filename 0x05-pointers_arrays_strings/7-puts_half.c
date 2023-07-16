b3d1b270399eaf6d67478fb0dd8abc572f300236
Mary-Maina <richellwanjiru@gmail.com>
2023-07-15 17:37:44 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half- Prints half a string
+ * @str: The string
+ * Return: Half a string
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int total = 0;
+	int x = 0;
+	int a;
+
+	while (str[i] != '\0')
+	{
+		total++;
+		i++;
+	}
+
+	x = total / 2;
+
+	if (total % 2 != 0)
+	{
+		x = (total + 1) / 2;
+	}
+
+	for (a = x; a <= total; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
