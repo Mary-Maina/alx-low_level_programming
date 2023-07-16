bdc6b415364058574c88a3ccce52dc4100d3b2b0
Mary-Maina <richellwanjiru@gmail.com>
2023-07-15 17:43:53 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,12 +19,12 @@ void puts_half(char *str)
 
 	x = total / 2;
 
-	if (total % 2 != 0)
+	if (x % 2 != 0)
 	{
-		x = (total + 1) / 2;
+		x = (total - 1) / 2;
 	}
 
-	for (a = x; a <= total; a++)
+	for (a = x + 1; a <= total; a++)
 	{
 		_putchar(str[a]);
 	}
