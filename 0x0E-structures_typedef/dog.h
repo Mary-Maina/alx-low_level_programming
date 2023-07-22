bbe4d616d7e1c62aa5a9ca1bd4cba6e36933616d
Mary-Maina <richellwanjiru@gmail.com>
2023-07-22 15:23:33 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- prints elements of a dog
+ * @name: Name of the dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
