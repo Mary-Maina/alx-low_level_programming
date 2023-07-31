4bfd1c0f5540e4f3163dc476164af15a36f68d7f
Mary-Maina <richellwanjiru@gmail.com>
2023-07-30 17:59:48 -0700

    Struct Initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,38 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int i, j, x, n;
-	int total = 0, sum = 0;
-
-	if (d == NULL)
-	{
-		return;
-	}
-	for (x = 0; name[x] != '\0'; x++)
-	{
-		total++;
-	}
-	for (n = 0; owner[n] != '\0'; n++)
-	{
-		sum++;
-	}
-	d->name = (char *)malloc((total + 1) * sizeof(char));
-	d->owner = (char *)malloc((sum + 1) * sizeof(char));
-
-	if (d->name == NULL || d->owner == NULL)
-	{
-		free(d->name);
-		free(d->owner);
-		return;
-	}
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		d->name[i] = name[i];
-	}
+	d->name = name;
 	d->age = age;
-
-	for (j = 0; owner[j] != '\0'; j++)
-	{
-		d->owner[j] = owner[j];
-	}
+	d->owner = owner;
 }
