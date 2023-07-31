0e2489c4c61c4c9b63d0e0e4597aa37770dab8e5
Mary-Maina <richellwanjiru@gmail.com>
2023-07-30 17:06:15 -0700

    Struct Pointer Initialization

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog- Initializes struct dog
+ * @d: Object variable
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	int i, j, x, n;
+	int total = 0, sum = 0;
+
+	if (d == NULL)
+	{
+		return;
+	}
+	for (x = 0; name[x] != '\0'; x++)
+	{
+		total++;
+	}
+	for (n = 0; owner[n] != '\0'; n++)
+	{
+		sum++;
+	}
+	d->name = (char *)malloc((total + 1) * sizeof(char));
+	d->owner = (char *)malloc((sum + 1) * sizeof(char));
+
+	if (d->name == NULL || d->owner == NULL)
+	{
+		return;
+	}
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		d->name[i] = name[i];
+	}
+	d->age = age;
+
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+		d->owner[j] = owner[j];
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,17 @@
  * @age: Age of dog
  * @owner: Owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
+/*Function Prototypes*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
