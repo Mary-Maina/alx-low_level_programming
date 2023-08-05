d274e1f86628b493b4df4c96e4754982f9b9a8c6
Mary-Maina <richellwanjiru@gmail.com>
2023-08-05 13:29:03 -0700

    Writing hello world in Assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+	hello db "Hello, Holberton",0
+	format db "%s",10,0
+section .text
+	extern printf
+
+global main
+main:
+	push rbp
+	mov rdi, format
+	mov rsi, hello
+	xor rax, rax
+	call printf
+	pop rbp
+	ret
